027e7d34fefa14f7c852ce2039c173196fab2b46
tomz123 <tomz12321@gmail.com>
2018-04-27 11:06:01 +1000

    Refactor person to use extends
    Refactor person to use extends

--- src/Adults.java
+++ src/Adults.java
@@ -1,99 +1,19 @@
-
 /**
  * Write a description of class Adult here.
  *
  * @author Jyh-woei Yang
- * @version 20/04/2018
+ * @version 27/04/2018
  */
-public class Adults implements Person
+public class Adults extends Person
 {
     // instance variables - replace the example below with your own
     
-
     /**
      * Constructor for objects of class Adult
      */
     public Adults(String name, int age, char gender, String status)
     {
         // initialise instance variables
-        super();
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.status = status;
-    }
-
-    public Adults()
-    {
-        // initialise instance variables
-        this.name = "";
-        this.age = 0;
-        this.gender = 'M';
-        this.status = "";
-    }
-    
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    @Override
-    public String getName()
-    {
-        // put your code here
-        return name;
-    }
-    
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    @Override
-    public int getAge()
-    {
-        // put your code here
-        return age;
-    }
-    
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    @Override
-    public char getGender()
-    {
-        // put your code here
-        return gender;
-    }
-    
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    @Override
-    public String getStatus()
-    {
-        // put your code here
-        return status;
-    }
-    
-        /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    @Override
-    public void setStatus(String Status)
-    {
-        // put your code here
-        this.status = status;
+        super(name,age,gender,status);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,22 +1,142 @@
 
 /**
- * Write a description of interface Person here.
+ * Write a description of class Person here.
  *
  * @author Jyh-woei Yang
- * @version 20/04/2018
+ * @version 27/04/2018
  */
-public interface Person
+public class Person
 {
-    public String name = "";
-    public int age = 0;
-    public char gender = 'M';
-    public String status = "";
+   // initialise instance variables
+   private String name;
+   private int age;
+   private char gender;
+   private String status;
     
-    public String getName();
-    public String getAge();
-    public char getGender();
-    public void setName(String name);
-    public void setAge(int age);
-    public void setGender(char gender);
-    public void setStatus(String status);
+    public Person(String name, int age, char gender, String status)
+    {
+        // initialise instance variables in constructor
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.status = status;
+    }
+    
+    /**
+     * A method to get name from Person
+     *
+     * @param
+     * @return a String of name
+     */
+
+    public String getName()
+    {
+        // put your code here
+        return name;
+    }
+    
+    /**
+     * A method to get age from Person
+     *
+     * @param
+     * @return an int of age
+     */
+
+    public int getAge()
+    {
+        // put your code here
+        return age;
+    }
+    
+    /**
+     * A method to get gender from Person
+     *
+     * @param
+     * @return a char of gender
+     */
+ 
+    public char getGender()
+    {
+        // put your code here
+        return gender;
+    }
+    
+    /**
+     * A method to get status from Person
+     *
+     * @param
+     * @return a String of status
+     */
+
+    public String getStatus()
+    {
+        // put your code here
+        return status;
+    }
+
+     /**
+     * A method to set a String of name
+     *
+     * @param  name  a String for a method
+     * @return 
+     */
+
+    public void setName(String name)
+    {
+        // put your code here
+        this.name = name;
+    }
+
+     /**
+     * A method to set a int of name
+     *
+     * @param  int  a int for a method
+     * @return 
+     */
+
+    public void setAge(int Age)
+    {
+        // put your code here
+        this.age = age;
+    }
+
+     /**
+     * A method to set a char of gender
+     *
+     * @param  char  a char for a method
+     * @return 
+     */
+
+    public void setGender(char Gender)
+    {
+        // put your code here
+        this.gender = gender;
+    }
+    
+     /**
+     * A method to set a String of status
+     *
+     * @param  String  a String for a method
+     * @return
+     */
+
+    public void setStatus(String Status)
+    {
+        // put your code here
+        this.status = status;
+    }
+
+   /**
+     * A method to display String lines of Person
+     *
+     * @param
+     * @return
+     */
+    public void displayPerson()
+    {
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Gender:"+gender);
+        System.out.println("Status:"+status);
+    }
 }
