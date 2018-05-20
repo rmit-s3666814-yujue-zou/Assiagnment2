63ea57462c7793d876fad20953ace5d226f2430d
tomz123 <tomz12321@gmail.com>
2018-05-21 03:35:47 +1000

    Tom-RelationshipDao-01
    Implement writeFile function

--- src/RelationshipDao.java
+++ src/RelationshipDao.java
@@ -52,15 +52,67 @@ public class RelationshipDao {
      * A method to pass result list to view
      * 
      * @param  
-     * @return a list of person
+     * @return a list of relationship
      */
     public ArrayList<Relationship> getResultList()
     {
     		return relationshipList;
     }
     
-//    private void writeFile(){
-//        String fileName = "Relationship.txt";
-//
-//    }
-}
+
+    /**
+     * A method to write relationship back to file
+     * 
+     * @param  
+     * @return
+     * @throws IOException while exception during I/O actions       
+     */      
+     
+     private void writeFile()
+     {
+    	 	String filename = ("Relationship.txt");
+    	 	String[] relationship = new String[3];
+    	 	Scanner input = new Scanner(System.in);
+    	 	String line = "";
+    	 	Driver newDriver = new Driver();
+        ArrayList<Relationship> toWriteRelationshipList = newDriver.getRelationshipDriverList();
+
+        //try catch to handle IOException
+        try
+        {
+            PrintWriter outputFile = new PrintWriter (filename);
+
+            for (int i = 0 ; i < toWriteRelationshipList.size() ; i++ )
+            {
+
+            		relationship[0] = toWriteRelationshipList.get(i).getPerson1().getName();
+                	relationship[1] = toWriteRelationshipList.get(i).getPerson2().getName();
+                	relationship[2] = toWriteRelationshipList.get(i).getRelationship();
+
+                 //combine elements into a line
+                 for (int k = 0 ; k < 3 ; k++ )
+                 {   
+                     //line = tickets[0] + "," + tickets[1] + "," + tickets[2];
+                     if (k != 2)
+                         line = line + relationship[k] + ",";
+                     else
+                         line = line + relationship[k];
+                 }
+                 //display a message about write line
+                 int lineCount = i + 1;
+                 //System.out.println("");
+                 //System.out.println("Write a message in line "+ lineCount +" to a file");
+                 //System.out.println("");
+
+                 outputFile.println(line);
+                 //reset line
+                 line = "";
+            }
+                outputFile.close();    
+            }
+            catch(IOException exception)
+            {
+                System.out.println("Unexpected I/O error occured");
+            }
+        }
+    }
